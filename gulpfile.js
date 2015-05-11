9182236f7c70693a37f9d9c4427e9ad9ea71fbe5
Aliaksei Krasikau <Aliaksei_Krasikau@epam.com>
2015-05-11 17:08:09 +0300

    Create index.html - web app start point. Create main.js - react start point Set up npm dependencies and gulp tasks.

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var concat = require('gulp-concat');
+
+gulp.task('browserify', function() {
+	gulp.src('src/client/js/main.js')
+		.pipe(browserify({transform: 'reactify'}))
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest('build/client/js'));
+});
+
+gulp.task('copy', function() {
+	gulp.src('src/client/index.html')
+		.pipe(gulp.dest('build/client'));
+});
+
+gulp.task('default', ['browserify', 'copy']);
+
+gulp.task('watch', function() {
+	gulp.watch('src/client/**/*.*', ['default']);
+});
---
+++ src/client/js/main.js
@@ -0,0 +1,3 @@
+var React = require('react');
+
+React.render(<h1>Welcome to auction</h1>, document.getElementById('main'));
